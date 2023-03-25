69dbb3573be55a95adadd381725f9aafed1de817
adusachev <usachev.alexe@gmail.com>
2023-03-25 11:57:34 +0300

    add KernelAdd prog

--- CUDA/KernelAdd.cu
+++ CUDA/KernelAdd.cu
@@ -2,29 +2,27 @@
 #include <cmath>
 
 
-__global__
-void KernelMul(int n, float* x, float* y, float* res) {
-	/*  
-        Поэлементное произведение двух массивов x и y
-	*/
-	int tid = (blockIdx.x * blockDim.x) + threadIdx.x;
+__global__ void KernelAdd(int numElements, float* x, float* y, float* result) {
+    int index = blockIdx.x * blockDim.x + threadIdx.x;
 	int stride = blockDim.x * gridDim.x;
 
-	for (int i = tid; i < n; i += stride) {
-		res[i] = x[i] * y[i];
+	for (int i = index; i < numElements; i += stride) {
+		result[i] = x[i] + y[i];
 	}
 }
 
 
-int main() {
-	int n = 1 << 26;  // 2**28
 
-	// step 1: allocate Host memory
+int main() {
+	int n = 1 << 28;
+
+    // step 1: allocate Host memory
 	float *h_x = new float[n];
 	float *h_y = new float[n];
 	float *h_res = new float[n];
 
-	// step 2: allocate Device memory
+
+    // step 2: allocate Device memory
 	float *d_x;
 	float *d_y;
 	float *d_res;
@@ -33,33 +31,29 @@ int main() {
 	cudaMalloc(&d_y, nbytes);
 	cudaMalloc(&d_res, nbytes);
 
-	// fill Host arrays
+	
+    // fill Host arrays
 	for (int i = 0; i < n; i++) {
-		h_x[i] = 2.0f;
-		h_y[i] = 3.0f;
+		h_x[i] = 1.0f;
+		h_y[i] = 2.0f;
 	}
 
-	// step 3: copy arrays from Host to Device (size in bytes!)
+    // step 3: copy arrays from Host to Device (size in bytes!)
 	cudaMemcpy(d_x, h_x, nbytes, cudaMemcpyHostToDevice);
 	cudaMemcpy(d_y, h_y, nbytes, cudaMemcpyHostToDevice);
 
-	// measure calculations time
+
+    // start measure calculations time
 	cudaEvent_t start, end;
 	float milliseconds;
 	cudaEventCreate(&start);
 	cudaEventCreate(&end);
-
 	cudaEventRecord(start);
 
-
-	// step 4: run calculations
+    // step 4: Kernel call
 	int blockSize = 256;
 	int numBlocks = (n + blockSize - 1) / blockSize;
-
-	int numBlocks = 1;
-	int blockSize = 1;  // blockSize <= 1024
-	KernelMul<<<numBlocks, blockSize>>>(n, d_x, d_y, d_res);
-
+	KernelAdd<<<numBlocks, blockSize>>>(n, d_x, d_y, d_res);
 
 	// end time measure
 	cudaEventRecord(end);
@@ -67,21 +61,19 @@ int main() {
 	// step 5: copy calc result from Device to Host
 	cudaMemcpy(h_res, d_res, nbytes, cudaMemcpyDeviceToHost);
 
-
-	cudaEventSynchronize(end);  // (!)
-
+	cudaEventSynchronize(end);
 	cudaEventElapsedTime(&milliseconds, start, end);
     std::cout << "Time elapsed: " << milliseconds << " ms " << std::endl;
 
-	// step 6: free Host memory
-	delete[] h_x;
-	delete[] h_y;
-	delete[] h_res;
+    // check answer
+	float maxError = 0.0f;
+	for (int i = 0; i < n; i++) {
+		maxError = fmax(maxError, fabs(h_res[i]-3.0f));
+	}
+	std::cout << "Max error: " << maxError << std::endl;
 
-	// step 7: free Device memory
 	cudaFree(d_x);
 	cudaFree(d_y);
-	cudaFree(d_res);
 
 
 	return 0;
